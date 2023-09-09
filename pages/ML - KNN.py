45fd18dad6d220164206a969c5bf0210e85feeba
Pedro <pedro.silva@surfix.com.br>
2023-09-09 18:47:45 -0300

    feat: graficos de knn

--- pages/ML - KNN.py
+++ pages/ML - KNN.py
@@ -1,15 +1,16 @@
+from pathlib import Path
+
 import numpy as np
 import pandas as pd
 import plotly.express as px
 import streamlit as st
-
-from pathlib import Path
 from sklearn.cluster import DBSCAN
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import silhouette_score, silhouette_samples
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import NearestNeighbors
 from sklearn.preprocessing import OneHotEncoder, StandardScaler
+
 from data import tratamento_dados
 
 # Configuração da página Streamlit
@@ -57,19 +58,24 @@ encoded_columns.columns = encoder.get_feature_names_out(['typing', 'shape', 'pri
 pokemon_features = pd.concat([pokemon_features, encoded_columns], axis=1)
 pokemon_features.drop(columns=['typing', 'shape', 'primary_color'], inplace=True)
 
-# Criação do gráfico de dispersão com clusters
-pokemon_features, pca1, pca2 = tratamento_dados.clusterizar_df()
-
-# Criação do gráfico de dispersão com clusters
-graph_clusters = px.scatter(pokemon_features, pca1, pca2, color='cluster_label',
-                            title='Gráfico de Clusters após PCA e Método do Cotovelo')
-graph_clusters = px.scatter(pokemon_features, pca1, pca2, color='cluster_label',
-                            title='Gráfico de Clusters após PCA e Método do Cotovelo')
-
 # Coeficiente de silhueta
 silhouette_avg = silhouette_score(pokemon_features_clusters, pokemon_features_clusters['cluster_label'])
 silhouette_values = silhouette_samples(pokemon_features_clusters, pokemon_features_clusters['cluster_label'])
 pokemon_features['silhouette_score'] = silhouette_values
+st.write(f"Coeficiente de Silhueta Médio no KNN com cluster: {silhouette_avg}")
+st.write(f"Coeficiente por cluster: {silhouette_avg}")
+# Criação do gráfico de dispersão com clusters
+pokemon_features, pca1, pca2 = tratamento_dados.clusterizar_df()
+# Criação do gráfico de dispersão com clusters
+graph_clusters = px.scatter(pokemon_features, pca1, pca2, color='cluster_label',
+                            title='Gráfico de Clusters após PCA e Método do Cotovelo')
+graph_clusters = px.scatter(pokemon_features, pca1, pca2, color='cluster_label',
+                            title='Gráfico de Clusters após PCA e Método do Cotovelo')
+st.plotly_chart(graph_clusters)
+silhouette_avg = silhouette_score(pokemon_features, pokemon_features['cluster_label'])
+sample_silhouette_values = silhouette_samples(pokemon_features, pokemon_features['cluster_label'])
+pokemon_features['silhouette_score'] = sample_silhouette_values
+st.write(pokemon_features)
 
 # Configuração do modelo KNN
 k_neighbors = 20
@@ -81,11 +87,9 @@ knn_modelC = NearestNeighbors(n_neighbors=k_neighbors, metric='euclidean')
 knn_modelC.fit(pokemon_features_clusters)
 
 # Preparação Random Forest
-X = pokemon_features 
+X = pokemon_features
 y = pokemon_df[['typing']]
 
-# Dividir os dados em conjuntos de treinamento e teste
-from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # Criar uma instância do modelo RandomForestClassifier
@@ -99,6 +103,7 @@ y_pred = rf_model.predict(X_test)
 
 # Avaliar o desempenho
 from sklearn.metrics import accuracy_score
+
 accuracy = accuracy_score(y_test, y_pred)
 
 # Preparação DBSCAN
@@ -109,6 +114,29 @@ scaled_numeric_df = scaler.fit_transform(pokemon_features)
 # Aplicar DBSCAN para agrupamento
 dbscan = DBSCAN(eps=5, min_samples=5, metric='euclidean')
 pokemon_clusters = dbscan.fit_predict(scaled_numeric_df)
+
+# Preparação do modelo Random Forest
+X = pokemon_features  # Seus recursos
+y = pokemon_df['typing']  # Sua coluna de destino (classes)
+
+# Dividir os dados em conjuntos de treinamento e teste
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Criar uma instância do modelo RandomForestClassifier
+rf_model = RandomForestClassifier(n_estimators=100, max_depth=10, random_state=42)
+
+# Treinar o modelo
+rf_model.fit(X_train, y_train)
+
+# Fazer previsões
+y_pred = rf_model.predict(X_test)
+
+# Avaliar o desempenho (vamos usar a precisão como exemplo)
+from sklearn.metrics import accuracy_score
+
+accuracy = accuracy_score(y_test, y_pred)
 
 # Início da página principal
 logo2 = current_path / "assets" / "icons" / "logo2.png"
@@ -121,25 +149,26 @@ st.markdown('<p class="site-subt"><b>K-Nearest Neighbors</b></p>', unsafe_allow_
 
 with st.expander("Recomendações de Pokémon"):
     pokemon_choose = st.selectbox('Escolha um Pokémon', pokemon_df['name'], help='Selecione um Pokémon que você gosta')
-    
+
     if pokemon_choose:
         selected_pokemon_index = pokemon_df[pokemon_df['name'] == pokemon_choose].index[0]
         distances, indices = knn_model.kneighbors(pokemon_features.iloc[selected_pokemon_index].values.reshape(1, -1))
-        
+
         # st.write(distances)
         st.subheader("Pokémon semelhantes:")
-        
+
         if pokemon_choose:
             selected_pokemon_index = pokemon_df[pokemon_df['name'] == pokemon_choose].index[0]
-            
+
             distances, indices = knn_model.kneighbors(
                 pokemon_features.iloc[selected_pokemon_index].values.reshape(1, -1))
             colunas = st.columns(8)
-            
+
             for i in range(8):
                 with colunas[i]:
                     st.header(f"{i + 1}º")
-                    st.image(pokemon_df.loc[indices[0][i+1], 'image'], caption=pokemon_df.loc[indices[0][i+1], 'name'],
+                    st.image(pokemon_df.loc[indices[0][i + 1], 'image'],
+                             caption=pokemon_df.loc[indices[0][i + 1], 'name'],
                              width=100)
 
 # Seção para K-Nearest Neighbors com Clusterização
@@ -148,21 +177,21 @@ st.markdown('<p class="site-subt"><b>K-Nearest Neighbors com Clusterização</b>
 with st.expander("Recomendações de Pokémon"):
     pokemon_choose = st.selectbox('Selecione um Pokémon', pokemon_df['name'],
                                   help='Selecione um Pokémon que você gosta')
-    
+
     if pokemon_choose:
         selected_pokemon_index = pokemon_df[pokemon_df['name'] == pokemon_choose].index[0]
-        
+
         distances, indices = knn_modelC.kneighbors(
             pokemon_features_clusters.iloc[selected_pokemon_index].values.reshape(1, -1))
-        
-        #st.write(distances)
+
+        # st.write(distances)
         st.subheader("Pokémon semelhantes:")
         colunas = st.columns(8)
-        
+
         for i in range(8):
             with colunas[i]:
                 st.header(f"{i + 1}º")
-                st.image(pokemon_df.loc[indices[0][i+1], 'image'], caption=pokemon_df.loc[indices[0][i+1], 'name'],
+                st.image(pokemon_df.loc[indices[0][i + 1], 'image'], caption=pokemon_df.loc[indices[0][i + 1], 'name'],
                          width=100)
 
 # Seção para Random Forest
@@ -170,21 +199,21 @@ st.markdown('<p class="site-subt"><b>Random Forest</b></p>', unsafe_allow_html=T
 
 with st.expander("Recomendações de Pokémon"):
     pokemon_choose_rf = st.selectbox('Opte por um Pokémon', pokemon_df['name'],
-                                  help='Selecione um Pokémon que você gosta')
-    
+                                     help='Selecione um Pokémon que você gosta')
+
     if pokemon_choose_rf:
         # Fazer previsão com o modelo Random Forest
         selected_pokemon_index_rf = pokemon_df[pokemon_df['name'] == pokemon_choose_rf].index[0]
         selected_pokemon_features_rf = X.iloc[selected_pokemon_index_rf].values.reshape(1, -1)
         predicted_typing_rf = rf_model.predict(selected_pokemon_features_rf)
-        
+
         # Filtrar Pokémon com o mesmo tipo previsto
         similar_pokemon_indices_rf = pokemon_df[pokemon_df['typing'] == predicted_typing_rf[0]].index
         similar_pokemon_indices_rf = similar_pokemon_indices_rf[similar_pokemon_indices_rf != selected_pokemon_index_rf]
 
         st.subheader("Pokémon semelhantes:")
         colunas_rf = st.columns(8)
-        
+
         for i in range(8):
             with colunas_rf[i]:
                 st.header(f"{i + 1}º")
@@ -195,7 +224,8 @@ with st.expander("Recomendações de Pokémon"):
 st.markdown('<p class="site-subt"><b>DBSCAN</b></p>', unsafe_allow_html=True)
 
 with st.expander("Recomendações de Pokémon"):
-    pokemon_choose_dbscan = st.selectbox('Selete um Pokémon', pokemon_df['name'], help='Selecione um Pokémon que você gosta')
+    pokemon_choose_dbscan = st.selectbox('Selete um Pokémon', pokemon_df['name'],
+                                         help='Selecione um Pokémon que você gosta')
 
     if pokemon_choose_dbscan:
         selected_pokemon_index_dbscan = pokemon_df[pokemon_df['name'] == pokemon_choose_dbscan].index[0]
@@ -216,39 +246,39 @@ with st.expander("Recomendações de Pokémon"):
                 st.image(pokemon_df.iloc[similar_pokemon_indices[i]]['image'],
                          caption=pokemon_df.iloc[similar_pokemon_indices[i]]['name'], width=100)
 
-
-st.markdown('<hr>', unsafe_allow_html=True) 
-
+st.markdown('<hr>', unsafe_allow_html=True)
 
 # Seção de comparação e Análise
 st.markdown('<h3 class="site-subt"><b>Análises e Comparações</b></h3>', unsafe_allow_html=True)
-with st.expander("KNN x KNN com clusterização"):
+with st.expander("KNN com clusterização"):
     st.write(f"Coeficiente de Silhueta Médio no KNN com cluster: {silhouette_avg}")
     st.write(f"Coeficiente por cluster: {silhouette_avg}")
     st.plotly_chart(graph_clusters)
-    
-    pokemon_choose = st.selectbox('Pokémon de Referência', pokemon_df['name'], help='Selecione um Pokémon que você gosta')
+
+    pokemon_choose = st.selectbox('Pokémon de Referência [Com cluster] ', pokemon_df['name'],
+                                  help='Selecione um Pokémon que você gosta')
 
     if pokemon_choose:
         selected_pokemon_index = pokemon_df[pokemon_df['name'] == pokemon_choose].index[0]
-        distances, indices = knn_model.kneighbors(pokemon_features.iloc[selected_pokemon_index].values.reshape(1, -1))
+        distances, indices = knn_modelC.kneighbors(
+            pokemon_features_clusters.iloc[selected_pokemon_index].values.reshape(1, -1))
 
         st.subheader("Pokémon semelhantes:")
         if pokemon_choose:
             selected_pokemon_index = pokemon_df[pokemon_df['name'] == pokemon_choose].index[0]
 
-            distances, indices = knn_model.kneighbors(
-                pokemon_features.iloc[selected_pokemon_index].values.reshape(1, -1))
+            distances, indices = knn_modelC.kneighbors(
+                pokemon_features_clusters.iloc[selected_pokemon_index].values.reshape(1, -1))
 
             # print(distances.mean())
             distances = distances[0]
             indices = indices[0]
             numberPokedexList = []
-            
+
             for i in range(len(indices)):
                 number_pokedex = pokemon_df.loc[indices[i], 'pokedex_number']
                 numberPokedexList.append(number_pokedex)
-            
+
             mean_distances = []
             data = {
                 'Índice na Pokedex': numberPokedexList,
@@ -272,6 +302,55 @@ with st.expander("KNN x KNN com clusterização"):
 
             # Crie um gráfico interativo com Plotly Express
             fig = px.line(df, x='Valor de k', y='Média das Distâncias', title='Gráfico de Distância')
-            st.plotly_chart(fig)   
-    
-st.markdown('<hr>', unsafe_allow_html=True) 
+            st.plotly_chart(fig)
+
+with st.expander("KNN sem clusterização"):
+    pokemon_choose = st.selectbox('Pokémon de Referência [Sem cluster]', pokemon_df['name'],
+                                  help='Selecione um Pokémon que você gosta')
+
+    if pokemon_choose:
+        selected_pokemon_index = pokemon_df[pokemon_df['name'] == pokemon_choose].index[0]
+        distances, indices = knn_model.kneighbors(pokemon_features.iloc[selected_pokemon_index].values.reshape(1, -1))
+
+        st.subheader("Pokémon semelhantes:")
+        if pokemon_choose:
+            selected_pokemon_index = pokemon_df[pokemon_df['name'] == pokemon_choose].index[0]
+
+            distances, indices = knn_model.kneighbors(
+                pokemon_features.iloc[selected_pokemon_index].values.reshape(1, -1))
+
+            # print(distances.mean())
+            distances = distances[0]
+            indices = indices[0]
+            numberPokedexList = []
+
+            for i in range(len(indices)):
+                number_pokedex = pokemon_df.loc[indices[i], 'pokedex_number']
+                numberPokedexList.append(number_pokedex)
+
+            mean_distances = []
+            data = {
+                'Índice na Pokedex': numberPokedexList,
+                'Distância': distances
+            }
+
+            df = pd.DataFrame(data)
+            fig = px.scatter(df, x='Distância', y='Índice na Pokedex',
+                             title='Distâncias entre Pontos Mais Próximos')
+
+            st.plotly_chart(fig)
+
+            mean_distances = []
+            k_max = 50  # Defina o valor máximo de k
+            for k in range(1, k_max + 1):
+                mean_distance_k = np.mean(distances[:k])
+                mean_distances.append(mean_distance_k)
+
+            # Crie um DataFrame para os dados do gráfico
+            df = pd.DataFrame({'Valor de k': range(1, k_max + 1), 'Média das Distâncias': mean_distances})
+
+            # Crie um gráfico interativo com Plotly Express
+            fig = px.line(df, x='Valor de k', y='Média das Distâncias', title='Gráfico de Distância')
+            st.plotly_chart(fig)
+
+st.markdown('<hr>', unsafe_allow_html=True)
